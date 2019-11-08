09f96d72f18d0ef65dff28b5ff5f60cc52b0c9aa
OlivierArgentieri <olivierargentieri@gmail.com>
2019-11-08 15:48:40 +0100

    create and generate Dll

---
+++ EditoolsUnity/EditoolsUnity/EditoolsButton.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditoolsUnity
+{
+    /// <summary>
+    /// Class button of Editools
+    /// </summary>
+    public static class EditoolsButton
+    {
+        /// <summary>
+        /// Create button without parameters in callback
+        /// </summary>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        public static void Button(string _label, Color _c, Action _callback)
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+                _callback?.Invoke();
+            GUI.color = Color.white;
+        }
+
+        /// <summary>
+        /// Create button with parameter in callback
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        /// <param name="_arg0"></param>
+        public static void Button<T>(string _label, Color _c, Action<T> _callback, T _arg0)
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+                _callback?.Invoke(_arg0);
+            GUI.color = Color.white;
+        }
+
+        /// <summary>
+        /// Create button with parameters in callback
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        /// <param name="_arg0"></param>
+        /// <param name="_arg1"></param>
+        public static void Button<T1, T2>(string _label, Color _c, Action<T1, T2> _callback, T1 _arg0, T2 _arg1)
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+                _callback?.Invoke(_arg0, _arg1);
+            GUI.color = Color.white;
+        }
+
+
+
+        /// <summary>
+        /// Create button with parameters in callback
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        /// <param name="_arg0"></param>
+        /// <param name="_arg1"></param>
+        /// <param name="_arg2"></param>
+        public static void Button<T1, T2, T3>(string _label, Color _c, Action<T1, T2, T3> _callback, T1 _arg0, T2 _arg1, T3 _arg2)
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+                _callback?.Invoke(_arg0, _arg1, _arg2);
+            GUI.color = Color.white;
+        }
+
+
+        /// <summary>
+        /// Create button and confirm box without parameters in callback
+        /// </summary>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        /// <param name="_titleConfirm"></param>
+        /// <param name="_textConfirm"></param>
+        /// <param name="_validTest"></param>
+        /// <param name="_cancelText"></param>
+        public static void ButtonWithConfirm(string _label, Color _c, Action _callback, string _titleConfirm, string _textConfirm, string _validTest = "Ok", string _cancelText = "Cancel")
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+            {
+                bool _confirm = EditorUtility.DisplayDialog(_titleConfirm, _textConfirm, _validTest, _cancelText);
+                if (_confirm)
+                    _callback?.Invoke();
+            }
+            GUI.color = Color.white;
+        }
+
+        /// <summary>
+        /// Create button and confirm box with parameter in callback
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        /// <param name="_arg0"></param>
+        /// <param name="_titleConfirm"></param>
+        /// <param name="_textConfirm"></param>
+        /// <param name="_validTest"></param>
+        /// <param name="_cancelText"></param>
+        public static void ButtonWithConfirm<T>(string _label, Color _c, Action<T> _callback, T _arg0, string _titleConfirm, string _textConfirm, string _validTest = "Ok", string _cancelText = "Cancel")
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+            {
+                bool _confirm = EditorUtility.DisplayDialog(_titleConfirm, _textConfirm, _validTest, _cancelText);
+                if (_confirm)
+                    _callback?.Invoke(_arg0);
+            }
+            GUI.color = Color.white;
+        }
+
+        /// <summary>
+        /// Create button and confirm box with parameters in callback
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        /// <param name="_arg0"></param>
+        /// <param name="_arg1"></param>
+        /// <param name="_titleConfirm"></param>
+        /// <param name="_textConfirm"></param>
+        /// <param name="_validTest"></param>
+        /// <param name="_cancelText"></param>
+        public static void ButtonWithConfirm<T1, T2>(string _label, Color _c, Action<T1, T2> _callback, T1 _arg0, T2 _arg1, string _titleConfirm, string _textConfirm, string _validTest = "Ok", string _cancelText = "Cancel")
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+            {
+                bool _confirm = EditorUtility.DisplayDialog(_titleConfirm, _textConfirm, _validTest, _cancelText);
+                if (_confirm)
+                    _callback?.Invoke(_arg0, _arg1);
+            }
+            GUI.color = Color.white;
+        }
+
+
+        /// <summary>
+        /// Create button and confirm box with parameters in callback
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <param name="_label"></param>
+        /// <param name="_c"></param>
+        /// <param name="_callback"></param>
+        /// <param name="_arg0"></param>
+        /// <param name="_arg1"></param>
+        /// <param name="_arg3"></param>
+        /// <param name="_titleConfirm"></param>
+        /// <param name="_textConfirm"></param>
+        /// <param name="_validTest"></param>
+        /// <param name="_cancelText"></param>
+        public static void ButtonWithConfirm<T1, T2, T3>(string _label, Color _c, Action<T1, T2, T3> _callback, T1 _arg0, T2 _arg1, T3 _arg2, string _titleConfirm, string _textConfirm, string _validTest = "Ok", string _cancelText = "Cancel")
+        {
+            GUI.color = _c;
+            if (GUILayout.Button(_label))
+            {
+                bool _confirm = EditorUtility.DisplayDialog(_titleConfirm, _textConfirm, _validTest, _cancelText);
+                if (_confirm)
+                    _callback?.Invoke(_arg0, _arg1, _arg2);
+            }
+            GUI.color = Color.white;
+        }
+    }
+
+    /// <summary>
+    /// Class field of Editools
+    /// </summary>
+    public static class EditoolsField
+    {
+        /// <summary>
+        /// Create Text field with out data
+        /// </summary>
+        /// <param name="_label"></param>
+        /// <param name="_output"></param>
+        public static void TextField(string _label, out string _output)
+        {
+            _output = EditorGUILayout.TextField(_label);
+        }
+
+        /// <summary>
+        /// Create Vector3 field with return vector3 data
+        /// </summary>
+        /// <param name="_label"></param>
+        /// <param name="_vector3"></param>
+        /// <returns></returns>
+        public static Vector3 Vector3Field(string _label, Vector3 _vector3)
+        {
+            return EditorGUILayout.Vector3Field(_label, _vector3);
+        }
+
+        /// <summary>
+        /// Create Color field with out data
+        /// </summary>
+        /// <param name="_color"></param>
+        /// <param name="_outColor"></param>
+        public static void ColorField(Color _color, out Color _outColor)
+        {
+            _outColor = EditorGUILayout.ColorField(_color);
+        }
+    }
+
+    /// <summary>
+    /// Class box of editools
+    /// </summary>
+    public static class EditoolsBox
+    {
+        /// <summary>
+        /// Create HelpBox
+        /// </summary>
+        /// <param name="_message"></param>
+        /// <param name="_messageType"></param>
+        public static void HelpBox(string _message, MessageType _messageType = MessageType.None)
+        {
+            EditorGUILayout.HelpBox(_message, _messageType);
+        }
+    }
+
+    /// <summary>
+    /// Class Layout of editools
+    /// </summary>
+    public static class EditoolsLayout
+    {
+        /// <summary>
+        /// Create Space with repeting parameters
+        /// </summary>
+        /// <param name="_i"></param>
+        public static void Space(int _i)
+        {
+            for (int i = 0; i < _i; i++)
+            {
+                EditorGUILayout.Space();
+            }
+        }
+
+        /// <summary>
+        /// Create Start or End GUILayout
+        /// </summary>
+        /// <param name="_begin"></param>
+        public static void Horizontal(bool _begin)
+        {
+            if (_begin) EditorGUILayout.BeginHorizontal();
+            else EditorGUILayout.EndHorizontal();
+        }
+    }
+}
